84de4224209152b9dda3a7031af61f447e8a590f
Mahmoud Alarby <MahmoudAlarby41@gmail.com>
2024-01-16 02:19:45 +0200

    first quiz

--- 01-quiz/main.go
+++ 01-quiz/main.go
@@ -17,30 +17,35 @@ var (
 )
 
 func main() {
+
+	// flags
 	var (
 		flagProblemsFilename = flag.String("p", ProblemsFile, "Problems file name")
 		flagTimer            = flag.Duration("t", 30*time.Second, "Time of the test")
 		flagShuffle          = flag.Bool("s", false, "Shuffle questions")
 	)
+	//parsing
 	flag.Parse()
-
-	// fmt.Println(flagTimer)
+	// flag validations
 	if flagProblemsFilename == nil || flagTimer == nil || flagShuffle == nil {
 		fmt.Println("Missing problems file name or timer")
 		return
 	}
-
 	fmt.Println("Press Enter to start Quiz from %q in %v?\n", *flagProblemsFilename, *flagTimer)
 	fmt.Scanln()
+	// opening csv content
 	f, err := os.Open(*flagProblemsFilename)
 	if err != nil {
 		fmt.Printf("Faild to open file: %v\n", err)
 		return
 	}
+	// close when finishing
 	defer f.Close()
+	// reading csv content
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	totalrecords = len(records)
+	// shuffling records (questions) array
 	if *flagShuffle {
 		rand.Shuffle(totalrecords, func(i, j int) {
 			records[i], records[j] = records[j], records[i]
@@ -55,6 +60,7 @@ func main() {
 
 	quizTimer := time.Tick(*flagTimer)
 
+	// chanal seclection
 	select {
 	case <-quizDone:
 	case <-quizTimer:
@@ -64,8 +70,10 @@ func main() {
 }
 
 func startQuiz(records [][]string) chan bool {
+	// make chanal
 	done := make(chan bool)
 
+	// go routine
 	go func() {
 		for i, record := range records {
 			question, correctAnswer := record[0], record[1]
